127d6011e8e61e621fd170dcf4994b2dd6608d1c
Al Zami Arafat <60238255+alzamiarafat@users.noreply.github.com>
2021-10-23 17:33:15 +0600

    Delete learning-nodejs-nodejs-practice-test/nodejs/practice-test directory

--- learning-nodejs-nodejs-practice-test/nodejs/practice-test/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const Bluebird = require('bluebird');
-
-const config = require('./config.json');
-
-const connection = mysql.createConnection(config.rds);
-connection.query = Bluebird.promisify(connection.query);
-
-module.exports = connection;
